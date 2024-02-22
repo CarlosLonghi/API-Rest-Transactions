5c1fb825f6ba943b5db045264b2bacf7398a61e5
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-02-22 14:40:25 -0300

    feat: use cookie in Fastify to save SessionId

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
 // Plugins
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
