6a7394a90810bde0a6be9c6f8fec6b4bca3eb2fb
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-02-23 09:29:59 -0300

    test: install supertest and create test createTransaction

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify()
-
-// Plugins
-app.register(cookie)
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
 
 app
   .listen({
--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,9 +1,24 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('O usuário consegue criar uma nova transação', () => {
-  // Fazendo a chamada HTTP
-  const responseStatusCode = 201
+// Await app is ready to run tests
+beforeAll(async () => {
+  await app.ready()
+})
 
-  // Validação
-  expect(responseStatusCode).toEqual(201)
+// Close app after run tests
+afterAll(async () => {
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 1000,
+      type: 'credit',
+    })
+    .expect(201)
 })
