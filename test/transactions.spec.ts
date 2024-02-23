736a742f585f0b9906d9cee34c92e8b5ce3a4ff9
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-02-23 17:26:39 -0300

    test: create transactions list test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -13,7 +13,10 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
-  test('User can create a new transaction', async () => {
+  // Um teste NÃO pode depender de outro teste!
+
+  // it types: it.skip(test is skiped), it.todo(test to do), it.only(run single test),
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -22,5 +25,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 1000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 1000,
+      }),
+    ])
   })
 })
