19920caddc3ae1e2bb6d2529043116505c980a4d
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-02-23 16:47:00 -0300

    test: create category test

---
+++ test/transactions.spec.ts
@@ -0,0 +1,26 @@
+import { test, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  // Await app is ready to run tests
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  // Close app after run tests
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('User can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 1000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
